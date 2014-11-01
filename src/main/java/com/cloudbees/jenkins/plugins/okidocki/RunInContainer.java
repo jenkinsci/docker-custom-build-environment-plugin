c6313a71703f839124558daaf0daf40fed414f54
Nicolas De Loof <nicolas.deloof@gmail.com>
2014-11-01 14:30:11 +0100

    use a single action to store build container refs + badge

--- src/main/java/com/cloudbees/jenkins/plugins/okidocki/RunInContainer.java
+++ src/main/java/com/cloudbees/jenkins/plugins/okidocki/RunInContainer.java
@@ -1,9 +1,11 @@
 package com.cloudbees.jenkins.plugins.okidocki;
 
 import hudson.Extension;
+import hudson.model.BuildBadgeAction;
 import hudson.model.InvisibleAction;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import hudson.model.listeners.RunListener;
 import hudson.model.listeners.SCMListener;
 import hudson.scm.ChangeLogSet;
 import hudson.scm.SCM;
@@ -12,11 +14,12 @@ import hudson.scm.SCM;
  * Used to determine if launcher has to be decorated to execute in container, after SCM checkout completed.
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
-public class RunInContainer extends InvisibleAction {
+public class RunInContainer implements BuildBadgeAction {
 
     private boolean enable;
 
     public String image;
+    public String container;
 
     public void afterSCM() {
         this.enable = true;
@@ -26,6 +29,23 @@ public class RunInContainer extends InvisibleAction {
         return enable;
     }
 
+    public String getIconFileName() {
+        return "/plugin/oki-docki/docker-badge.png";
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getDisplayName() {
+        return "build inside docker container";
+    }
+
+    public String getUrlName() {
+        return "/docker";
+    }
+
+
     @Extension
     public static class Listener extends SCMListener {
         @Override
@@ -34,4 +54,5 @@ public class RunInContainer extends InvisibleAction {
             if (runInContainer != null) runInContainer.afterSCM();
         }
     }
+
 }
