5043a23c8a98c6a758de0558d04fdd5486fbafc5
Christophe Labouisse <christophe@labouisse.org>
2014-12-07 13:29:51 +0100

    Added volumes from support: Fixes #9
    - Added new volumesFrom parameter in Docker.runDetached
    - Use volumesFrom when the system property oki-docki.running.from.container is defined

--- src/main/java/com/cloudbees/jenkins/plugins/okidocki/Docker.java
+++ src/main/java/com/cloudbees/jenkins/plugins/okidocki/Docker.java
@@ -8,6 +8,7 @@ import hudson.util.ArgumentListBuilder;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
 
 /**
@@ -70,7 +71,7 @@ public class Docker {
             throw new RuntimeException("Failed to remove docker container "+container);
     }
 
-    public String runDetached(String image, String workdir, Map<String, String> volumes, EnvVars environment, String user, String ... command) throws IOException, InterruptedException {
+    public String runDetached(String image, String workdir, Map<String, String> volumes, Collection<String> volumesFrom, EnvVars environment, String user, String... command) throws IOException, InterruptedException {
 
         ArgumentListBuilder args = new ArgumentListBuilder();
         args.add("docker", "run", "-t", "-d", "-u", user, "-w", workdir);
@@ -80,6 +81,10 @@ public class Docker {
         for (Map.Entry<String, String> e : environment.entrySet()) {
             args.add("-e");
             args.addMasked(e.getKey()+"="+e.getValue());
+        }
+        for (String container : volumesFrom) {
+            args.add("--volumes-from");
+            args.add(container);
         }
         args.add(image).add(command);
 
