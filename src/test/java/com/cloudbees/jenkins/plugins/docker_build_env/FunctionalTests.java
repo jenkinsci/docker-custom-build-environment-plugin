020efcdb28a2eabf432f258b2aabb8ac4fda7315
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-06-28 08:20:46 +0200

    Merge branch 'JENKINS-28989'

---
+++ src/test/java/com/cloudbees/jenkins/plugins/docker_build_env/FunctionalTests.java
@@ -0,0 +1,43 @@
+package com.cloudbees.jenkins.plugins.docker_build_env;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Result;
+import hudson.tasks.Shell;
+import org.apache.commons.io.FileUtils;
+import org.jenkinsci.plugins.docker.commons.credentials.DockerServerEndpoint;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class FunctionalTests {
+
+    @Rule  // @ClassRule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    @Test
+    public void run_inside_pulled_container() throws Exception {
+        FreeStyleProject project = jenkins.createFreeStyleProject();
+
+        project.getBuildWrappersList().add(
+            new DockerBuildWrapper(
+                new PullDockerImageSelector("ubuntu:14.04"),
+                "", new DockerServerEndpoint("", ""), "", true, false)
+        );
+        project.getBuildersList().add(new Shell("lsb_release  -a"));
+
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        jenkins.assertBuildStatus(Result.SUCCESS, build);
+        String s = FileUtils.readFileToString(build.getLogFile());
+        assertThat(s, containsString("Ubuntu 14.04"));
+        jenkins.buildAndAssertSuccess(project);
+    }
+
+}
