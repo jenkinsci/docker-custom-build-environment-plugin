2970fd83c144d162d3f291dd264596498017da32
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-11-27 15:58:04 +0100

    Revert "maintain overrides from wrapped ProcStarter"
    This reverts commit 824ae14289242aaab7070650298c374ca2c63e5a.
    
    Most plugins do run `launch().cmds(cmd).envs(envVars).start()` passing the whole environment they get from `build.getEnvironment()`
    as a result this propagates the whole host environment inside container.

--- src/main/java/com/cloudbees/jenkins/plugins/docker_build_env/DockerDecoratedLauncher.java
+++ src/main/java/com/cloudbees/jenkins/plugins/docker_build_env/DockerDecoratedLauncher.java
@@ -45,12 +45,6 @@ public class DockerDecoratedLauncher extends Launcher.DecoratedLauncher {
 
         try {
             EnvVars environment = buildContainerEnvironment();
-            // Apply environment overrides as set to original ProcStarter
-            for (String override : starter.envs()) {
-                int i = override.indexOf('=');
-                environment.override(override.substring(0,i), override.substring(i+1));
-            }
-
             runInContainer.getDocker().executeIn(runInContainer.container, userId, starter, environment);
         } catch (InterruptedException e) {
             throw new IOException("Caught InterruptedException", e);
@@ -70,8 +64,6 @@ public class DockerDecoratedLauncher extends Launcher.DecoratedLauncher {
         for (Environment e : build.getEnvironments()) {
             e.buildEnvVars(environment);
         }
-
-
 
         return environment;
     }
